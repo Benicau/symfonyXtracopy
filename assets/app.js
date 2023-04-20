f2d84cf1dfac5c88ce5ba3e95a4811d3384be9a5
Benicau <benoit_caucheteux@hotmail.com>
2023-04-21 00:22:57 +0200

    fin 21-04

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,53 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+
+
+
+// js site
+
+//effet triange carre homme page
+var randX = []
+var randY = []
+var rotation = 0
+function move ()
+{
+    for(var x=0;x<2;x++)
+    {
+            var chemin = ".layer"+x
+        $(chemin).animate({left: randX[x]+"%", top: randY[x]+"%"},10000, randomImage)  
+    }
+}
+function randomImage ()
+{
+    for(var i=0;i<2;i++)
+    {
+        randX[i]=Math.floor(Math.random()*100)
+        randY[i]=Math.floor(Math.random()*100)
+        if(randX[i]>80){randX[i]=80}
+        if(randY[i]>80){randY[i]=80}
+        if(randX[i]<20){randX[i]=20}
+        if(randY[i]<20){randY[i]=20}
+     
+    }
+    move ()
+
+}
+function rotationImage () {   
+    rotation++
+    for (var i=0;i<2;i++)
+    {   
+            var chemin = ".layer"+i
+    var rotationLien = "rotate("+rotation+"deg)"
+    $(chemin).css("transform",rotationLien)
+    } 
+    if (rotation==360){rotation=0}  
+}
+
+setInterval(function () {rotationImage()}, 20);
+randomImage ()
+
+
+
